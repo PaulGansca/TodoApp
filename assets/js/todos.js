8316b1f5411e56bcec937adabf05e0891f6bdefe
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-05-20 16:19:28 +0100

    Added the functionality

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,1 +1,23 @@
-alert("Connected")
+//Check off specific todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//Click on X to delete Todo
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+//Adding todos
+$("input[type='text']").keypress(function(event){
+	if (event.which === 13) {
+		//getting new todo
+		var todoText = $(this).val();
+		$(this).val("");
+		//create new li with the todo
+		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+	}
+});
