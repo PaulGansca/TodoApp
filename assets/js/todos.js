55412af8ef663e39e036574f193c50135aed64ab
Gansca Paul Mihai <gansca.paul@gmail.com>
2018-05-20 20:47:16 +0100

    Styled todo list and added transition animations

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -18,6 +18,11 @@ $("input[type='text']").keypress(function(event){
 		var todoText = $(this).val();
 		$(this).val("");
 		//create new li with the todo
-		$("ul").append("<li><span>X </span>" + todoText + "</li>");
+		$("ul").append("<li><span><i class='fa fa-trash'></i></span>" + todoText + "</li>");
 	}
+});
+
+//toggle todo input
+$(".fa-plus").click(function(){
+	$("input[type='text'").fadeToggle();
 });
